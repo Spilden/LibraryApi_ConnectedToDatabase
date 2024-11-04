56255ddeb834a87800ae9c0bd8d456c4a71f72f1
Rejwan Raouf <rejwan_raouf@outlook.com>
2024-11-04 19:04:31 +0100

    First commit of library

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}
